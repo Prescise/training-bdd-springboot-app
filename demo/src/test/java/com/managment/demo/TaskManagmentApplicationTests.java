ef7cc695bc17061cd431b6a68cfea4018149bfdd
PrescilliaS <prescillia.seck@lafoncierenumerique.com>
2022-03-25 17:21:44 +0100

    first commit

---
+++ demo/src/test/java/com/managment/demo/TaskManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
